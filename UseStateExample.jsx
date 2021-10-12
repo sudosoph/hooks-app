9f1c5040a28c10230250b0c1bdff27aa95969f0b
Sophia <sophiaraji@gmail.com>
2021-10-11 20:24:52 -0400

    Created form

--- UseStateExample.jsx
+++ UseStateExample.jsx
@@ -18,7 +18,30 @@ const UseStateExample = () => {
     evt.preventDefault();
     setShowMessage(true);
   };
-  return <></>;
+  return (
+  <>
+      <p>Complete the form below and see a nice message about yourself!</p>
+      <form onSubmit={handleSubmit}>
+            <label htmlFor="name">What should we call you?</label>
+            <input id="name" value={formValues['name']} onChange={handleChange} />
+
+            <label htmlFor="age">How old are you?</label>\
+            <input id="age" value={formValues['age'] onChange={handleChange} />
+
+            <label htmlFor='fruit'>What's your favorite fruit?</label>
+            <select id='fruit' value={formValues['fruit']} onChange={handleChange}>
+                <option>Bananas</option>
+                <option>Apples</option>
+                <option>Oranges</option>
+                <option>Strawberries</option>
+                <option>Pears</option>
+                <option>Dragonfruit</option>
+            </select>
+
+            <button>Submit</button>
+      </form>
+  </>
+  );
 };
 
 export default UseStateExample;
