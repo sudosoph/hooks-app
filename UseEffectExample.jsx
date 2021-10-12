d2a842fb57c7b50041f08c87c776c067d8df3e85
Sophia <sophiaraji@gmail.com>
2021-10-11 21:26:11 -0400

    Added message to UseStateExample and built out Dog API in UseEffect

---
+++ UseEffectExample.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+
+const UseEffectExample = () => {
+  const apiBaseUrl = "https://dog.ceo/api/breeds/image/random";
+  const [dogImageUrls, setDogImageUrls] = useState([]);
+  const [loadPictures, setLoadPictures] = useState(false);
+
+  const loadDogPictures = () => {
+    const numToLoad = Math.random() * 10;
+    fetch(`${apiBaseUrl}${numToLoad}`)
+      .then((response) => response.json())
+      .then((data) => setDogImageUrls(data.message));
+  };
+
+  useEffect(() => {
+    loadDogPictures();
+  }, []);
+
+  useEffect(() => {
+    if (loadDogPictures) {
+      loadDogPictures();
+      setLoadPictures(false);
+    }
+  }, [loadPictures]);
+  return (
+    <>
+      <h2>Random Dog Pictures</h2>
+      <button onClick={() => setLoadPictures(true)}>
+        Load some more pictures!
+      </button>
+      <p>
+        {dogImageUrls.length <= 0
+          ? "...loading"
+          : `We've got ${dogImageUrls.length} pictures for you`}
+      </p>
+      <div className="dogs-container">
+        {dogImageUrls.map((imgUrl) => (
+          <img
+            key={imgUrl}
+            src={imgUrl}
+            alt="A great picture of a random dog"
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default UseEffectExample;
--- UseStateExample.jsx
+++ UseStateExample.jsx
@@ -26,7 +26,7 @@ const UseStateExample = () => {
             <input id="name" value={formValues['name']} onChange={handleChange} />
 
             <label htmlFor="age">How old are you?</label>\
-            <input id="age" value={formValues['age'] onChange={handleChange} />
+            <input id="age" value={formValues['age']} onChange={handleChange} />
 
             <label htmlFor='fruit'>What's your favorite fruit?</label>
             <select id='fruit' value={formValues['fruit']} onChange={handleChange}>
@@ -40,6 +40,19 @@ const UseStateExample = () => {
 
             <button>Submit</button>
       </form>
+      {showMessage && (
+          <div className="form-message">
+              <p>
+                  Hi <strong>{formValues['name']}</strong>, what a lovely name. And I
+                  can't believe you're only <strong>{formValues['age']}</strong> years
+                  old, that's no age!
+              </p>
+              <p>
+                  Your favorite fruit can't be <strong>{formValues['fruit']}</strong>,
+                  mine too!
+              </p>
+          </div>
+      )}
   </>
   );
 };
